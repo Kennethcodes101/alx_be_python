9902a220709e548bd42b49bb50f84d4e4bb3b7c0
Kenneth Njaramba <kennsincebirth@gmail.com>
2025-09-12 18:12:48 +0300

    Correction for Savings calculation for finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your monthly expenses: "))
-savings_per_month = income - expenses 
-Projected_Savings = savings_per_month * 12 + (savings_per_month * 12 * 0.05)
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses 
+Projected_Savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 print ("Your monthly savings are $",savings_per_month,".")
 print("Projected savings after one year, with interest, is: $",int(Projected_Savings),".")
