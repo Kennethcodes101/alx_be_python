1f7347ac6d4ff096c25b465169a4828763bdde1c
Kenneth Njaramba <kennsincebirth@gmail.com>
2025-09-23 10:04:20 +0300

    New files just for the fun of coding

---
+++ python_introduction/challenge.py
@@ -0,0 +1,25 @@
+rows = 5
+i = 1
+
+while i <= rows:
+   j = 1
+   while j <= i:
+      print('#' , end= '')
+      j =+ 1
+
+   k = 1
+   while k < 2*(rows  - i):
+      print(' ' , end= '')
+      k = k + 1
+
+      l = 1
+      while l <= i:
+         print('#' , end= '')
+         l = l + 1          
+
+      print()
+i = i + 1
+   
+
+
+   
