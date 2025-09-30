30b9081e9c903487776924772397046b29f61488
Kenneth Njaramba <kennsincebirth@gmail.com>
2025-09-30 12:06:04 +0300

    Time correctly formatted in explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,13 +2,13 @@ from datetime import datetime
 
 def display_current_datetime():
     current_date = datetime.datetime.now()
-    print("Current date and time:", current_date)
+    print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 def calculate_future_date():
     
     number_of_days = int(input("Enter the number of days to add to the current date: "))
     future_date = datetime.datetime.now() + datetime.timedelta(days=number_of_days)
-    print(f"Future Date:", future_date)
+    print(f"Future Date:", future_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 
 display_current_datetime()
